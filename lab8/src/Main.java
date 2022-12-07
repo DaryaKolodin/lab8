63a6800dc7731af5832394eea8c4891b3e857ada
Asus <kaktotak778@gmail.com>
2022-12-07 22:24:21 +0300

    reset

--- lab8/src/Main.java
+++ lab8/src/Main.java
@@ -8,7 +8,6 @@ import java.util.*;
 
 public class Main {
 
-    //pism govno
     public static void main(String[] args) throws Exception {
         ArrayList<String> list = new ArrayList<>();
         System.out.println(" -- ADD --"); // 1
