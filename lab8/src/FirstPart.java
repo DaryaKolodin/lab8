8841fc03274baf8c43b875b0a49ab2102b56100e
Asus <kaktotak778@gmail.com>
2022-12-08 16:55:18 +0300

    add test method

--- lab8/src/FirstPart.java
+++ lab8/src/FirstPart.java
@@ -5,6 +5,9 @@ import java.util.TreeMap;
 
 public class FirstPart {
 
+    public void Test(){
+        // test method
+    }
 
     public static void addAndDelete(ArrayList<String> list) {
         list.add("Lesha");
